d945345f166cf85d84f5797edaf57279b404e280
ANKE-SaiPrasad <89138270+ankesai8@users.noreply.github.com>
2022-01-03 09:57:01 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -7,8 +7,7 @@ export default function Footer() {
                 <div className="container text-muted">
                     <h3 className="text-uppercase font-weight-bold text-center"><u>Stay healthy - Stay fit</u></h3>
                     <br />
-                    <p className=" text-center p-font ">This website is developed by Avikal Goel, Student of Delhi Technological
-                        University, 2023 batch, to assist people to track their daily fitness schedule and to stay healthy. It
+                    <p className=" text-center p-font ">This website is developed by ANKE SAIPRASAD, to assist people to track their daily fitness schedule and to stay healthy. It
                         displays all relevant details to the users. This dashboard uses React for frontend development, MongoDB for storing the user data and Express.JS for the backend work.
                     </p>
 
